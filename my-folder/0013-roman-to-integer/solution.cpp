88319e68b40db835cc04bf78c0b888a968ee7721
Mahanya <mahannya.kochhar@gmail.com>
2021-02-21 07:11:00 +0000

    [LeetCode Sync] Runtime - 4 ms (65.81%), Memory - 6 MB (100.00%)

---
+++ my-folder/0013-roman-to-integer/solution.cpp
@@ -0,0 +1,96 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+    	int l=s.length();
+        int sum=0;
+    	for(int i=0;i<s.length();i++)
+    	{
+    		if(s[i]=='I')
+    		{
+    			if(i+1<s.length())
+    			{
+    				if(s[i+1]=='V')
+    				{
+    					sum=sum+4;
+    					i++;
+    				}
+    				else if(s[i+1]=='X')
+    				{
+    					sum=sum+9;
+    					i++;
+    				}
+    				else
+    					sum=sum+1;
+    			}
+    			else
+    				sum+=1;
+
+    		}
+    		else if(s[i]=='V')
+    		{
+    			sum=sum+5;
+    		}
+    		else if(s[i]=='X')
+    		{
+    			if(i+1<s.length())
+    			{
+    				if(s[i+1]=='L')
+    				{
+    					sum=sum+40;
+    					i++;
+    				}
+    				else if(s[i+1]=='C')
+    				{
+    					sum=sum+90;
+    					i++;
+    				}
+    				else
+    					sum=sum+10;
+    				
+    			}
+    			else
+    			{
+    				sum+=10;
+
+    			}
+
+    		}
+    		else if(s[i]=='L')
+    			sum=sum+50;
+    		else if(s[i]=='C')
+    		{
+    			if(i+1<s.length())
+    			{
+    				if(s[i+1]=='D')
+    				{
+    					sum=sum+400;
+    					i++;
+    				}
+    				else if(s[i+1]=='M')
+    				{
+    					sum=sum+900;
+    					i++;
+    				}
+    				else
+    					sum=sum+100;
+    				
+    			}
+    			else
+    			{
+    				sum+=100;
+
+    			}
+
+    		}
+    		else if(s[i]=='D')
+    			sum=sum+500;
+    		else if(s[i]=='M')
+    			sum=sum+1000;
+
+
+    	}
+    	return sum;
+
+        
+    }
+};
