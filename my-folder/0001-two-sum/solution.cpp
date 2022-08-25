dcb11f0d8055ab82e83cb4c2c823b398c1e8d5fa
Mahanya <mahannya.kochhar@gmail.com>
2022-08-25 07:56:06 +0000

    [LeetCode Sync] Runtime - 4 ms (53.53%), Memory - 11.1 MB (99.98%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -2,53 +2,20 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        /*
-        
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
+        map<int,int>mp;
+        vector<int>ans;
+        for(int j=0;j<nums.size();j++)
         {
-            int t=target;
-            t=t-nums[i];
-            auto it=lower_bound(nums.begin(),nums.end(),t);
-            if(*it==t)
+            int t=target-nums[j];
+            if(mp.find(t)!=mp.end())
             {
-                vector<int>ans{i,int(it-nums.begin())};
+                
+                ans.push_back(j);
+                ans.push_back(mp[t]);
                 return ans;
             }
+            mp[nums[j]]=j;
         }
-        return jlt;
-    } */
-        vector<int>jlt;
-       vector<pair<int,int>>p;
-        for(int i=0;i<nums.size();i++)
-        {
-            p.push_back({nums[i],i});
-        }
-        sort(p.begin(),p.end());
-        vector<int>temp;
-        for(int i=0;i<p.size();i++)
-        {
-            temp.push_back({p[i].first});
-        }
-        for(int i=0;i<temp.size();i++)
-        {
-            int t=target-temp[i];
-            auto it2=temp.begin()+i;
-            auto it=lower_bound(it2+1,temp.end(),t);
-            if(it!=temp.end()&&*it==t)
-            {
-                int idx=it-temp.begin();
-                vector<int>ans{p[i].second,p[idx].second};
-                return ans;
-            }
-            auto it3=lower_bound(temp.begin(),it2-1,t);
-            if(it3!=temp.end()&&*it3==t)
-            {
-                int idx=it3-temp.begin();
-                vector<int>ans{p[i].second,p[idx].second};
-                return ans;
-            }
-        }
-        return jlt;
+        return ans;
     }
 };
