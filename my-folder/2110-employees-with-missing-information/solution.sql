d35c9e9f029ba269b8c134d1ed0f62bf7bb6a2df
Mahanya <mahannya.kochhar@gmail.com>
2022-12-19 07:12:17 +0000

    [LeetCode Sync] Runtime - 536 ms (52.36%), Memory - 0.0B (100.00%)

---
+++ my-folder/2110-employees-with-missing-information/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+Select Employees.employee_id as employee_id from Employees
+Left Join Salaries ON
+Employees.employee_id=Salaries.employee_id
+where salary is NULL
+Union
+Select Salaries.employee_id as employee_id from Employees
+Right Join Salaries ON
+Employees.employee_id=Salaries.employee_id
+where name is NULL
+order by employee_id
