4a4d2fa0d7891f7d27bddb592463d9b31179dc16
Mahanya <mahannya.kochhar@gmail.com>
2023-01-10 05:20:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.1 MB (99.96%)

--- my-folder/0100-same-tree/solution.cpp
+++ my-folder/0100-same-tree/solution.cpp
@@ -11,12 +11,17 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) 
+    bool isSameTree(TreeNode* p, TreeNode* q)
     {
+        if(p==NULL&&q==NULL)
+            return true;
         if(p==NULL||q==NULL)
-            return p==q;
-        if((*p).val!=(*q).val)
-          return false;
-        return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+            return false;
+        if(p->val==q->val)
+        {
+            return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+        }
+        else
+            return false;
     }
 };
