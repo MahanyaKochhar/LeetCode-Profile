5a81a516a7d5a6f30e2ebbce0b1dd4b370148c13
Mahanya <mahannya.kochhar@gmail.com>
2023-01-13 10:31:54 +0000

    [LeetCode Sync] Runtime - 113 ms (15.41%), Memory - 26.3 MB (100.00%)

---
+++ my-folder/0732-my-calendar-iii/solution.cpp
@@ -0,0 +1,28 @@
+class MyCalendarThree {
+public:
+    map<int,int>mp;
+    MyCalendarThree()
+    {
+        
+    }
+    
+    int book(int startTime, int endTime)
+    {
+        mp[startTime]++;
+        mp[endTime]--;
+        int ans=1;
+        int cnt=0;
+        for(auto x:mp)
+        {
+            cnt+=x.second;
+            ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(startTime,endTime);
+ */
