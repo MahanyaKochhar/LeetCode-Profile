aa19cfaf989a73354504d353faaf9152e7de7270
Mahanya <mahannya.kochhar@gmail.com>
2023-01-27 09:51:56 +0000

    [LeetCode Sync] Runtime - 735 ms (5.94%), Memory - 192.1 MB (20.30%)

--- my-folder/2627-difference-between-maximum-and-minimum-price-sum/solution.cpp
+++ my-folder/2627-difference-between-maximum-and-minimum-price-sum/solution.cpp
@@ -1,60 +1,60 @@
+bool cmp(const pair<int,int>&a,const pair<int,int>&b)
+{
+    return a.first > b.first;
+}
 class Solution {
 public:
     long long maxOutput(int n, vector<vector<int>>& edges, vector<int>& price)
     {
-        vector<vector<int>>adjlist(n);
-        for(int i=0;i<edges.size();i++)
+        vector<vector<int>> adjlist(n);
+        for(int i = 0;i < edges.size(); i++)
         {
             adjlist[edges[i][0]].push_back(edges[i][1]);
             adjlist[edges[i][1]].push_back(edges[i][0]);
         }
-        vector<long long int>f(n),g(n);
-        for(int i=0;i<n;i++)
+        long long dp[n][2];
+        memset(dp, 0, sizeof(dp));
+        long long ans=0;
+        auto dfs=[&](auto self, int V, int pv)->void
         {
-            f[i]=price[i];
-            g[i]=0;
-        }
-        auto dfs=[&](auto self,int V,int pv)->void
-        {
-            for(auto u:adjlist[V])
-            {
-                if(u==pv)
-                    continue;
-                self(self,u,V);
-                if(f[u]+price[V]>f[V])
-                {
-                    g[V]=f[V];
-                    f[V]=f[u]+price[V];
 
-                }
-                else if(g[V]<f[u]+price[V])
+            dp[V][1] = price[V];
+            vector<pair<long long,long long>>p1, p2;
+            for(auto u : adjlist[V])
+            {
+                if(u == pv)
+                    continue;
+                self(self, u, V);
+                p1.push_back({dp[u][1],u});
+                p2.push_back({dp[u][0],u});
+                dp[V][1] = max(dp[V][1], dp[u][1] + price[V]);
+                dp[V][0] = max(dp[V][0], dp[u][0] + price[V]);
+            }
+            sort(p1.begin(),p1.end(),cmp);
+            sort(p2.begin(),p2.end(),cmp);
+            if(p1.size()==0)
+            {
+                // ans=max(ans, (long long) price[V]);
+            }
+            else if(p1.size() == 1)
+            {
+                ans = max(ans, p2[0].first + price[V]);
+                ans = max(ans, p1[0].first);
+            }
+            else if(p1.size() > 1)
+            {
+                if(p1[0].second != p2[0].second)
                 {
-                    g[V]=f[u]+price[V];
+                    ans=max(ans,p2[0].first + p1[0].first + price[V]);
+                }
+                else
+                {
+                    long long maxi=max(p1[0].first + p2[1].first + price[V], price[V] + p1[1].first + p2[0].first);
+                    ans = max(ans, maxi);
                 }
             }
         };
         dfs(dfs,0,-1);
-        vector<long long int>h(n);
-        auto dfs2=[&](auto self,int V,int pv)->void
-        {
-            for(auto u:adjlist[V])
-            {
-                if(u==pv)
-                    continue;
-                if(f[u]+price[V]!=f[V])
-                    h[u]=max(h[V]+price[u],f[V]+price[u]);
-                else
-                     h[u]=max(h[V]+price[u],g[V]+price[u]);
-                self(self,u,V);
-            }
-        };
-        dfs2(dfs2,0,-1);
-        long long int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            long long int maxi=max(f[i]-price[i],h[i]-price[i]);
-            ans=max(ans,maxi);
-        }
         return ans;
     }
 };
