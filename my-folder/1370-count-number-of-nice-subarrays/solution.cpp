51fa98ea9f4357cc41c0c57be4a6fb7b8ebd9adb
Mahanya <mahannya.kochhar@gmail.com>
2023-03-08 07:09:39 +0000

    [LeetCode Sync] Runtime - 323 ms (5.01%), Memory - 93.5 MB (28.11%)

---
+++ my-folder/1370-count-number-of-nice-subarrays/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k)
+    {
+        map < int , int > mp;
+        int odd_count = 0;
+        mp[0] = 1;
+        int ans = 0;
+        for(int i = 0; i < nums.size() ; i++)
+        {
+            if(nums[i] % 2 != 0)
+                odd_count++;
+            mp[odd_count]++;
+            if(odd_count >= k)
+                ans += mp[odd_count - k];
+        }
+        return ans;
+    }
+};
