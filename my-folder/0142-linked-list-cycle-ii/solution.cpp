7a03b02a749b786c5219a41255ed7a0018cc0482
Mahanya <mahannya.kochhar@gmail.com>
2023-03-09 07:25:33 +0000

    [LeetCode Sync] Runtime - 8 ms (49.38%), Memory - 7.5 MB (100.00%)

---
+++ my-folder/0142-linked-list-cycle-ii/solution.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        if(head == NULL)
+            return NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        do
+        {
+            if(fast->next == NULL || fast->next->next == NULL)
+                return NULL;
+            slow = slow->next;
+            fast = fast ->next->next;
+        }while(slow != fast);
+        while(head != fast)
+        {
+            head = head->next;
+            fast = fast->next;
+        }
+        return head;
+    }
+};
