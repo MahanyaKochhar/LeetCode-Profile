a01cb3f4e7ec2563cc45b58525632771252e5158
Mahanya <mahannya.kochhar@gmail.com>
2023-05-15 06:02:56 +0000

    [LeetCode Sync] Runtime - 355 ms (5.36%), Memory - 259.7 MB (100.00%)

---
+++ my-folder/2792-neighboring-bitwise-xor/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived)
+    {
+        int res = 0;
+        for(int i = 0 ; i < derived.size() ; i++)
+        {
+            res = res ^ derived[i];
+        }
+        if(res  == 0)
+            return true;
+        else
+            return false;
+    }
+};
