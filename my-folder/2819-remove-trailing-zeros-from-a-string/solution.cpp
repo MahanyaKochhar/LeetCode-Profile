9675b29374076ecc4c64cd0c71de73c17da3f00b
Mahanya <mahannya.kochhar@gmail.com>
2023-05-28 02:35:52 +0000

    [LeetCode Sync] Runtime - 13 ms (6.83%), Memory - 9.4 MB (100.00%)

---
+++ my-folder/2819-remove-trailing-zeros-from-a-string/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeTrailingZeros(string num)
+    {
+        int i = num.length() - 1;
+        while(i>=0 && num[i] == '0')
+        {
+            i--;
+        }
+        string ans = "";
+        for(int j = 0 ; j <= i ; j++ )
+            ans.push_back(num[j]);
+        return ans;
+    }
+};
