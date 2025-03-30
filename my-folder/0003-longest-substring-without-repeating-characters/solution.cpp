7f90fa0cfb35e736b53a800ffdd81229eeede5dc
Mahanya <mahannya.kochhar@gmail.com>
2025-03-30 12:03:46 +0000

    [LeetCode Sync] Runtime - 7 ms (63.20%), Memory - 11.9 MB (54.08%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,49 +1,28 @@
 class Solution {
 public:
-    bool good(int mid,string s)
-    {
-        map<char,int>mp;
-        for(int i = 0 ; i < mid; i++)
-        {
-            mp[s[i]]++;
-        }
-        int st = 0;
-        for(int i = mid ; i < s.length();i++)
-        {
-            if(mp.size() == mid)
-                return true;
-            mp[s[st]]--;
-            if(mp[s[st]] == 0)
-                mp.erase(s[st]);
-            mp[s[i]]++;
-            st++;
-        }
-        if(mp.size() == mid)
-            return true;
-        else
-            return false;
-    }
-
     int lengthOfLongestSubstring(string s)
     {
-        if (s.length() == 0)
+        map<char,int>mp;
+        int st = 0;
+        int ans = 0;
+        for(int i = 0 ; i < s.length() ; i++)
         {
-            return 0;
-        }
-
-        int l = 1, r = s.length() + 1;
-        while(l + 1 < r)
-        {
-            int mid = (l + r) / 2;
-            if(good(mid,s))
+            if(mp.find(s[i]) == mp.end())
             {
-                l = mid;
+                mp[s[i]] = i;
+            }
+            else if(mp[s[i]] + 1 > st)
+            {
+                ans = max(ans, i - st);
+                st = mp[s[i]] + 1 ;
+                mp[s[i]] = i;
             }
             else
             {
-                r = mid;
+                mp[s[i]] = i;
             }
         }
-        return l;
+        ans = max(ans, (int)s.length() - st);
+        return ans;
     }
 };
