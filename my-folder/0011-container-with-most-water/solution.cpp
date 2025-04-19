7a5819ed2895f590a34a7278efe3a69bc89a1f25
Mahanya <mahannya.kochhar@gmail.com>
2025-04-19 08:20:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 62.9 MB (47.73%)

--- my-folder/0011-container-with-most-water/solution.cpp
+++ my-folder/0011-container-with-most-water/solution.cpp
@@ -1,71 +1,21 @@
 class Solution {
 public:
-    int maxArea(vector<int>& a) {
-    	int ans=INT_MIN;
-    	int l=0,r=a.size()-1;
-    	while(l<r)
-    	{
-    		ans=max(ans,min(a[l],a[r])*(r-l));
-    		int val=min(a[l],a[r]);
-            if(a[l]==a[r])
+    int maxArea(vector<int>& height)
+    {
+        int l = 0 , r = height.size() - 1;
+        int ans = 0;
+        while(l < r)
+        {
+            ans = max(ans, min(height[l],height[r]) * (r - l));
+            if(height[l] > height[r])
             {
-                if(a[l+1]>val)
-                {
-                    if(a[r-1]>val)
-                    {
-                        if(a[l+1]>a[r-1])
-                            l++;
-                        else
-                            r--;
-                    }
-                    else
-                        l++;
-                    continue;
-                }
-                else
-                {
-                    if(a[r-1]>val)
-                    {
-                        r--;
-                    }
-                    else
-                    {
-                        l++;
-                        r--;
-                    }
-                    continue;
-                }
-                
+                r--;
             }
-    		if(val==a[l])
-    		{
-                
-    			if(a[l+1]>a[l])
-    			{
-    				l++;
-    				continue;
-    			}
-            
-    			else
-                {
-                    l++;
-                    
-                }
-    		}
-    		else if(val==a[r])
-    		{
-                
-    			if(a[r-1]>a[r])
-    			{
-    				r--;
-    			}
-    			else
-                {
-                    
-                    r--;
-                }
-    		}
-    	}
-    	return ans;
+            else
+            {
+                l++;
+            }
+        }
+        return ans;
     }
 };
