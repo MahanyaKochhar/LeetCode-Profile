f8e4f4f15af90b22c16ced6489ba3b3b2a9c55c0
Mahanya <mahannya.kochhar@gmail.com>
2025-05-08 16:33:02 +0000

    [LeetCode Sync] Runtime - 841 ms (24.30%), Memory - 214.7 MB (21.83%)

---
+++ my-folder/3628-find-minimum-time-to-reach-last-room-ii/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime)
+    {
+        vector<vector<int>>d(moveTime.size(),vector<int>(moveTime[0].size(),INT_MAX));
+        d[0][0] = 0;
+        priority_queue<pair<int,pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,{0,0}}});
+        while(!pq.empty())
+        {
+            auto it = pq.top();
+            int curr = -it.first;
+            int move = it.second.first;
+            int i = it.second.second.first;
+            int j = it.second.second.second;
+            pq.pop();
+            if(d[i][j] < curr)
+            {
+                continue;
+            }
+
+            vector<int>x = {1,-1,0,0};
+            vector<int>y = {0,0,1,-1};
+            for(int k = 0 ; k < x.size();k++)
+            {
+                int u = x[k] + i;
+                int v = y[k] + j;
+                if(u < 0 || u >= moveTime.size() || v < 0 || v >= moveTime[0].size())
+                {
+                    continue;
+                }
+                int nowMax = max(curr,moveTime[u][v]) + ((move % 2 == 0) ? 1 : 2);
+                if(nowMax < d[u][v])
+                {
+                    d[u][v] = nowMax;
+                    pq.push({-nowMax,{move + 1,{u,v}}});
+                }
+            }
+        }
+        return d[moveTime.size() - 1][moveTime[0].size() - 1];
+    }
+};
