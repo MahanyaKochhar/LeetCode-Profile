dc6943f1a103859e4dc65016ad90622a28f825b4
Mahanya <mahannya.kochhar@gmail.com>
2025-05-11 04:23:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.8 MB (57.19%)

---
+++ my-folder/1293-three-consecutive-odds/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr)
+    {
+        int cnt = 0;
+        for(auto x : arr)
+        {
+            if(x % 2 == 0)
+            {
+                cnt = 0;
+            }
+            else
+            {
+                cnt++;
+                if(cnt == 3)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
