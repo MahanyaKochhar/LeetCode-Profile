2d0c78f709c1d5367a826f2d2e68ee615dbeeca9
Mahanya <mahannya.kochhar@gmail.com>
2025-07-14 15:29:31 +0000

    [LeetCode Sync] Runtime - 9 ms (61.23%), Memory - 23.6 MB (34.53%)

--- my-folder/0494-target-sum/solution.cpp
+++ my-folder/0494-target-sum/solution.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target)
     {
-        int dp[21][4001];
-        memset(dp,false,sizeof(dp));
         int sum = 0;
-        
-        for(auto x : nums)
+        for(auto &x : nums)
         {
             sum += x;
         }
-        if(sum < abs(target))
+        int req = (sum + target) / 2;
+        if(req < 0 || (sum + target) % 2  != 0)
         {
             return 0;
         }
-        dp[0][sum] = 1;
-        for(int i = 1 ; i <= nums.size();i++)
+        vector<vector<int>>dp(nums.size() + 1,vector<int>(2 * sum + 1,0));
+        dp[0][0] = 1;
+        for(int i = 1 ; i <= nums.size(); i++)
         {
-            for(int j = 0 ; j <= 2 * sum; j++)
+            for(int j = 0 ; j <= req ; j++)
             {
-                dp[i][j] += dp[i - 1][j + nums[i - 1]];
+                dp[i][j] = dp[i - 1][j];
                 if(j - nums[i - 1] >= 0)
                 {
                     dp[i][j] += dp[i - 1][j - nums[i - 1]];
                 }
             }
         }
-        return dp[nums.size()][target + sum];
+        return dp[nums.size()][req];
+
     }
 };
