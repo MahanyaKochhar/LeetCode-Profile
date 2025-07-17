2b4e82d5ae8442be8d2ec826f71369ac200d42f6
Mahanya <mahannya.kochhar@gmail.com>
2025-07-17 10:55:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15 MB (70.88%)

--- my-folder/0138-copy-list-with-random-pointer/solution.cpp
+++ my-folder/0138-copy-list-with-random-pointer/solution.cpp
@@ -1,4 +1,4 @@
-      /*
+/*
 // Definition for a Node.
 class Node {
 public:
@@ -16,53 +16,27 @@ public:
 
 class Solution {
 public:
-    map<Node*,int>mp1;
-    map<int,Node*>mp2;
-    Node* head1,*tail1;
-    Node* curr,*cpy;
-    Node* copyRandomList(Node* head) {
-        curr=head;
-        listing(head);
-        return head1;
-    }
-        void listing(Node* head)
-        {
-            int i=0;
-        while(head!=NULL)
-        {
-        if(head1==NULL)
-        {
-            head1=new Node(head->val);
-            mp1[head]=i;
-
-            tail1=head1;
-            mp2[i]=tail1;
-           i++;
-            head=head->next;
-            continue;
-        }
-        Node* temp=new Node(head->val);
-        mp1[head]=i;
-        mp2[i]=temp;
-        tail1->next=temp;
-        tail1=tail1->next;
-        head=head->next;
-        i++;
-        
-    }
-     cpy=head1;
-     while(curr!=NULL)
+    Node* copyRandomList(Node* head)
     {
-        if(curr->random==NULL)
-            cpy->random=NULL;
-        else
+        Node* dummy = new Node(0);
+        Node* prev = dummy;
+        Node* curr = head;
+        map<Node*,Node*>mp;
+        while(curr != NULL)
         {
-            int t=mp1[curr->random];
-            Node* tempo=mp2[t];
-            cpy->random=tempo;
-
+            Node* newNode = new Node(curr->val);
+            mp[curr] = newNode;
+            newNode->random = curr;
+            prev->next = newNode;
+            prev = newNode;
+            curr = curr->next;
         }
-        cpy=cpy->next;
-        curr=curr->next;
-    } 
-}};
+        Node* newCurr = dummy->next;
+        while(newCurr != NULL)
+        {
+            newCurr->random  = mp[newCurr->random->random];
+            newCurr = newCurr->next;
+        }
+        return dummy->next;
+    }
+};
