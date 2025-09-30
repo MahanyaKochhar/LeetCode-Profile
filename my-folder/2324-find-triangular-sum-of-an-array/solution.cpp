99ed1b50b585f0c2a71d2233d28d6d22e7917c04
Mahanya <mahannya.kochhar@gmail.com>
2025-09-30 00:15:02 +0000

    [LeetCode Sync] Runtime - 358 ms (19.65%), Memory - 382.7 MB (6.98%)

---
+++ my-folder/2324-find-triangular-sum-of-an-array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums)
+    {
+        int ans = 0;
+        while(nums.size() != 1)
+        {
+            vector<int> newNums;
+            for(int i = 0 ; i < nums.size() - 1; i++)
+            {
+                newNums.push_back((nums[i] + nums[i + 1]) % 10);
+            }
+            nums = newNums;
+        }
+        return nums[0];
+    }
+};
