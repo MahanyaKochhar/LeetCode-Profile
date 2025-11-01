0a4fa52c8a87236d10503866f4cacc0e6e013abb
Mahanya <mahannya.kochhar@gmail.com>
2025-11-01 23:56:05 +0000

    [LeetCode Sync] Runtime - 159 ms (14.61%), Memory - 266 MB (18.98%)

---
+++ my-folder/3501-delete-nodes-from-linked-list-present-in-array/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head)
+    {
+        set<int>s(nums.begin(),nums.end());
+        ListNode* dummy = new ListNode(-1,head);
+        ListNode* curr = dummy;
+        while(curr->next != NULL)
+        {
+            if(s.contains(curr->next->val))
+            {
+                curr->next = curr->next->next;
+            }
+            else
+            {
+                curr = curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
