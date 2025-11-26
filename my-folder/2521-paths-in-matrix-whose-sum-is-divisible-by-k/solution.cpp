a139b926a8082f150ccd40ce6fb75eabbd5bb650
Mahanya <mahannya.kochhar@gmail.com>
2025-11-26 00:18:13 +0000

    [LeetCode Sync] Runtime - 60 ms (93.22%), Memory - 57.7 MB (94.86%)

--- my-folder/2521-paths-in-matrix-whose-sum-is-divisible-by-k/solution.cpp
+++ my-folder/2521-paths-in-matrix-whose-sum-is-divisible-by-k/solution.cpp
@@ -5,14 +5,14 @@ public:
     {
         int dp[grid.size() + 1][grid[0].size() + 1][k];
         memset(dp, 0 ,sizeof(dp));
-        dp[1][0][0] = 1;
+        dp[0][1][0] = 1;
         for(int i = 1 ; i <= grid.size() ; i++)
         {
             for(int j = 1 ; j <= grid[0].size() ; j++)
             {
                 for(int l = 0 ; l < k ; l++)
                 {
-                    dp[i][j][(l)] = (dp[i - 1][j][(l  - grid[i - 1][j - 1] % k + k) % k] + dp[i][j - 1][(l - grid[i - 1][j - 1] % k + k) % k]) % mod;
+                    dp[i][j][(l + grid[i - 1][j - 1]) % k] = (dp[i - 1][j ][(l)] + dp[i][j - 1][l]) % mod;
                 }
             }
         }
