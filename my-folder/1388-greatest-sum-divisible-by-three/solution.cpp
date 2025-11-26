ec9f93bcc7344c5cb4ab1e70a9462c3ffd0f861f
Mahanya <mahannya.kochhar@gmail.com>
2025-11-26 00:03:04 +0000

    [LeetCode Sync] Runtime - 44 ms (14.49%), Memory - 42.5 MB (12.48%)

--- my-folder/1388-greatest-sum-divisible-by-three/solution.cpp
+++ my-folder/1388-greatest-sum-divisible-by-three/solution.cpp
@@ -2,47 +2,16 @@ class Solution {
 public:
     int maxSumDivThree(vector<int>& nums)
     {
-        vector<vector<int>>divs(3);
-        int sum = 0;
-        for(int i = 0 ; i < nums.size(); i++)
+        vector<vector<int>>dp(nums.size() + 1,vector<int>(3,INT_MIN));
+        dp[0][0] = 0;
+        for(int i = 1 ; i <= nums.size(); i++)
         {
-            int div = nums[i] % 3;
-            sum += nums[i];
-            divs[div].push_back(nums[i]);
-        }
-        sort(divs[1].begin(),divs[1].end());
-        sort(divs[2].begin(),divs[2].end());
-        int remove = 0;
-        if(sum % 3 == 0)
-        {
-            remove = 0;
-        }
-        else if(sum % 3 == 1)
-        {
-            int mini = INT_MAX;
-            if(divs[1].size() > 0)
+            for(int j = 0 ; j < 3 ; j++)
             {
-                mini = min(mini,divs[1][0]);
+                int k = (j + nums[i - 1]) % 3;
+                dp[i][k] = max(dp[i - 1][k],dp[i - 1][j] + nums[i - 1]);
             }
-            if(divs[2].size() > 1)
-            {
-                mini = min(mini,divs[2][0] + divs[2][1]);
-            }
-            remove = mini;
         }
-        else
-        {
-            int mini = INT_MAX;
-            if(divs[2].size() > 0)
-            {
-                mini = min(mini,divs[2][0]);
-            }
-            if(divs[1].size() > 1)
-            {
-                mini = min(mini,divs[1][0] + divs[1][1]);
-            }
-            remove = mini;
-        }
-        return sum - remove;
+        return dp[nums.size()][0];
     }
 };
